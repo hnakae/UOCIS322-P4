913be1a6059e4a56da13bfa8bd14094379a26961
Hiro <32496402+hnakae@users.noreply.github.com>
2021-06-10 19:45:09 -0700

    working

--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -1,8 +1,3 @@
-"""
-Replacement for RUSA ACP brevet time calculator
-(see https://rusa.org/octime_acp.html)
-
-"""
 import os
 import flask
 from flask import redirect, url_for, request, render_template
@@ -95,6 +90,33 @@ def _calc_times():
 
 
     print(result)
+
+    # import os
+    # from flask import Flask, redirect, url_for, request, render_template
+    # from pymongo import MongoClient
+    #
+    # app = Flask(__name__)
+    #
+    # client = MongoClient('mongodb://' + os.environ['MONGODB_HOSTNAME'], 27017)
+    # db = client.tododb
+    #
+    # @app.route('/')
+    # def index():
+    #     return render_template('index.html',
+    # 			  items=list(db.tododb.find()))
+    #
+    # @app.route('/insert/', methods=['POST'])
+    # def insert():
+    #     item_doc = {
+    #         'title': request.form['title'],
+    #         'body': request.form['body']
+    #     }
+    #     db.tododb.insert_one(item_doc)
+    #
+    #     return redirect(url_for('index'))
+    #
+    # if __name__ == "__main__":
+    #     app.run(host='0.0.0.0', debug=True)
 
 
 
